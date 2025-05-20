fec690902f2a12b6c5446e4ace56ce96305eeaff
Baptiste Guerin <guerinbaptiste@yahoo.fr>
2025-05-20 12:35:10 +0200

    feat(main): extract unique categories from works using Set
    Extracted unique categories by filtering duplicates based on category IDs.
    
    Used Set to track seen IDs and ensure only the first occurrence of each category is kept.
    
    This prepares for dynamic generation of category filters or UI elements.

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -13,6 +13,22 @@ import { displayWorks } from "./scripts/dom.js";
 (async function init() {
 
   const works = await fetchData("http://localhost:5678/api/works");
+
+  const allCategories = works.map(work => work.category);
+  const uniqueCategoryIds = new Set();
+
+  const uniqueCategories = allCategories.filter(category => {
+    const isDuplicate = uniqueCategoryIds.has(category.id);
+    uniqueCategoryIds.add(category.id);
+    return !isDuplicate;
+  });
+  console.log(uniqueCategories);
+
+  // Version concise 'Map'
+  // const uniqueCategories = [
+  //   ...new Map(works.map(work => [work.category.id, work.category])).values()
+  // ];
+
   
   displayWorks(works);
 })();
